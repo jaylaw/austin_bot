6ec1338b7292d502989c2d077c34fcc5e235afa8
J. Lawton <jaylaw1@gmail.com>
2017-10-21 16:25:04 -0800

    refactor: Add and comment helper functions
      - /echo and /done callback function repeat same session query.
      - comments added to functions to help with potential encapsulation

--- bot.py
+++ bot.py
@@ -19,45 +19,47 @@ updater = Updater(token=settings.TOKEN)
 dispatcher = updater.dispatcher
 
 
+# Callback function for the /start CommandHandler
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id,
                      text="I'm a bot, please talk to me!")
 
 
+# Callback function for the /done CommandHandler
 def done(bot, update):
-    assignments = [hw for hw, in session.query(Assignment.assignment)]
+    chat = update.message.chat_id
+    assignments = get_user_assignments(chat)
     reply_markup = ReplyKeyboardMarkup(
         build_menu(get_data_buttons(assignments),
                    n_cols=2), one_time_keyboard=True)
-    bot.send_message(chat_id=update.message.chat_id,
+    bot.send_message(chat_id=chat,
                      text="Which item did you complete?",
                      reply_markup=reply_markup)
 
 
+# Callback function for the /echo MessageHandler
 def echo(bot, update):
     chat = update.message.chat_id
     text = update.message.text
-    assignments = [hw for hw, in session.query(
-        Assignment.assignment).filter(Assignment.owner == chat)]
+    assignments = get_user_assignments(chat)
     if text in assignments:
         session.query(Assignment).filter(Assignment.assignment == text,
                                          Assignment.owner == chat).delete()
         session.commit()
         bot.send_message(chat_id=chat,
                          text=f'Removed "{text}" from your list of assignments')
-        assignments = [hw for hw, in session.query(
-            Assignment.assignment).filter(Assignment.owner == chat)]
+        assignments = get_user_assignments(chat)
     else:
         session.add(Assignment(assignment=text, owner=chat))
         session.commit()
-        assignments = [hw for hw, in session.query(
-            Assignment.assignment).filter(Assignment.owner == chat)]
+        assignments = get_user_assignments(chat)
 
     complete = '\n'.join(assignments)
-    bot.send_message(chat_id=update.message.chat_id,
+    bot.send_message(chat_id=chat,
                      text=complete)
 
 
+# Helper function to build ReplyMarkupKeyboard
 def build_menu(buttons,
                n_cols,
                header_buttons=None,
@@ -70,9 +72,19 @@ def build_menu(buttons,
     return menu
 
 
+# Helper function to build  button array for ReplyMarkupKeyboard
 def get_data_buttons(data):
     button_list = [s for s in data]
     return button_list
+
+
+# Helper function to get homework assignments for owner.
+def get_user_assignments(owner):
+    homework = []
+    for hw, in session.query(
+            Assignment.assignment).filter(Assignment.owner == owner):
+        homework.append(hw)
+    return homework
 
 
 def main():
