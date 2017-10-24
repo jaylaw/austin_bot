237803dfd5267f9d2a25ae63d3af9d58a97b8d9e
J. Lawton <jaylaw1@gmail.com>
2017-10-24 12:47:59 -0800

    Add settings.py

---
+++ settings.py
@@ -0,0 +1,12 @@
+TOKEN = '479353195:AAFcYPTG6lmXtDAJNyuXSdXF9oVYPjVokFM'
+
+DATABASE = {
+    'drivername': 'postgresql+psycopg2',
+    'host': 'localhost',
+    'port': '5432',
+    'username': 'webadmin',
+    'password': '***REMOVED***',
+    'database': 'test'
+}
+
+
